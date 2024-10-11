dd341c5aec24a5ba5c3e63304b5f0121a961b5d5
mayssajebali <144730448+mayssajebali@users.noreply.github.com>
2024-10-11 21:26:11 +0100

    modification de front et back

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -22,6 +22,12 @@ import { ListetachesComponent } from './prof_dashboard/listetaches/listetaches.c
 import { AssignerTacheComponent } from './prof_dashboard/assigner-tache/assigner-tache.component';
 import { SidebarProfComponent } from './prof_dashboard/sidebar-prof/sidebar-prof.component';
 import { NavbarProfComponent } from './prof_dashboard/navbar-prof/navbar-prof.component';
+import { DashboardEtdComponent } from './dashboard_Etd/dashboard-etd/dashboard-etd.component';
+import { NavbarEtdComponent } from './dashboard_Etd/navbar-etd/navbar-etd.component';
+import { SidebarEtdComponent } from './dashboard_Etd/sidebar-etd/sidebar-etd.component';
+import { CreertacheComponent } from './dashboard_Etd/creertache-perso/creertache.component';
+import { ListetachepersoComponent } from './dashboard_Etd/listetacheperso/listetacheperso.component';
+import { CreergroupeComponent } from './prof_dashboard/creergroupe/creergroupe.component';
 
 
 @NgModule({
@@ -46,6 +52,12 @@ import { NavbarProfComponent } from './prof_dashboard/navbar-prof/navbar-prof.co
     AssignerTacheComponent,
     SidebarProfComponent,
     NavbarProfComponent,
+    DashboardEtdComponent,
+    NavbarEtdComponent,
+    SidebarEtdComponent,
+    CreertacheComponent,
+    ListetachepersoComponent,
+    CreergroupeComponent,
   
    
   ],
