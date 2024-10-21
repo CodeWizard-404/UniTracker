d1b9cd535bfd41704a8152fba0f90aec356736c4
yosr2003 <yosr.amamou@yahoo.com>
2024-10-21 23:13:20 +0100

    creer groupe

--- BackEnd/src/main/java/com/dsi/projet/services/GroupeServiceImpl.java
+++ BackEnd/src/main/java/com/dsi/projet/services/GroupeServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import com.dsi.projet.entities.Etudiant;
 import com.dsi.projet.entities.Groupe;
+import com.dsi.projet.entities.Matiere;
 import com.dsi.projet.repositories.GroupeRepository;
+import com.dsi.projet.repositories.MatiereRepository;
 import com.dsi.projet.repositories.EtudiantRepository;
 
 
@@ -20,20 +22,34 @@ public class GroupeServiceImpl implements GroupeService {
 	private GroupeRepository Repository;
 	@Autowired
 	private EtudiantRepository EtudiantRepository;
+	@Autowired
+    private MatiereRepository matiereRepository;
 	
 	@Override
 	public Groupe addGroupe(Groupe g) {
+	  
 	    List<Integer> etudiantIds = g.getEtudiants(); 
 	    List<Etudiant> etudiants = new ArrayList<>();
-
+	    
 	    for (Integer etudiantId : etudiantIds) {
 	        EtudiantRepository.findById(etudiantId).ifPresent(etudiant -> {
-	            etudiant.getGroupes().add(g); 
-	            etudiants.add(etudiant); 
+	            etudiant.getGroupes().add(g);
+	            etudiants.add(etudiant);      
 	        });
 	    }
+
+	    // Associer les étudiants récupérés au groupe
 	    g.setEtudiants(etudiantIds); 
-	    return Repository.save(g); 
+
+	    // Récupérer la matière à partir de son ID, si elle est présente
+	    if (g.getMatiere()!= 0) {
+	        Matiere matiere = matiereRepository.findById(g.getMatiere())
+	                .orElseThrow(() -> new IllegalArgumentException("Matiere not found with id: " + g.getMatiere()));
+	        g.setMatiere(matiere);  // Associe la matière au groupe
+	    }
+
+	    // Sauvegarder le groupe avec les étudiants et la matière associés
+	    return Repository.save(g);
 	}
 
 
