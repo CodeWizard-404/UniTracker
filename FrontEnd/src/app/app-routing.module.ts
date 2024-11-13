7a12f00da74844934bca246ac3f908f5adc66319
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2024-11-14 00:19:03 +0100

    .

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -67,8 +67,8 @@ const routes: Routes = [
 
   // Student Routes
   { path: 'dashEtd', title: 'Étudiant', component: DashboardEtdComponent,canActivate: [EtudiantGuard] },
-  { path: 'dashEtd/:id/creertacheperso', title: 'Créer Tâche Personnalisée', component: CreertacheComponent,canActivate: [EtudiantGuard] },
-  { path: 'dashEtd/:id/listetacheperso', title: 'Liste des Tâches Personnalisées', component: ListetachepersoComponent,canActivate: [EtudiantGuard] },
+  { path: 'dashEtd/:id/creertacheperso', title: 'Créer Tâche ', component: CreertacheComponent,canActivate: [EtudiantGuard] },
+  { path: 'dashEtd/:id/listetacheperso', title: 'Liste des Tâches', component: ListetachepersoComponent,canActivate: [EtudiantGuard] },
   { path: 'creerGroupe', title: 'Créer Groupe', component: CreergroupeComponent ,canActivate: [EtudiantGuard]},
   { path: 'dashboardEtud/:id/taches/:idTache', title: 'Modifier Tâche', component: ModifierTacheComponent,canActivate: [EtudiantGuard] },
 
