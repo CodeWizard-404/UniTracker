dad109b4d87ce8a8247a9411a811d9e74a54e0d5
yosr2003 <yosr.amamou@yahoo.com>
2024-11-14 21:29:02 +0100

    fhth

--- FrontEnd/src/app/dashboard_Etd/sidebar-etd/sidebar-etd.component.ts
+++ FrontEnd/src/app/dashboard_Etd/sidebar-etd/sidebar-etd.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EtudiantServiceService } from 'src/app/services/etudiant-service.service';
 
 @Component({
   selector: 'app-sidebar-etd',
@@ -8,8 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SidebarEtdComponent implements OnInit{
   idEtudiant!:number;
-  constructor(private route:ActivatedRoute){}
+  name!: string;
+  constructor(private route:ActivatedRoute, private etudiantServ: EtudiantServiceService){}
   ngOnInit(): void {
     this.idEtudiant = Number(this.route.snapshot.paramMap.get('id'));
+    this.etudiantServ.getEtudiantById(this.idEtudiant).subscribe(
+      (response) => {
+        console.log('prof', response);
+        this.name = response.nom_Etd + ' ' + response.prenom_Etd;
+  
+      },
+      (error) => {
+        console.error('Erreur', error);
+     
+      }
+    );
   }
 }
